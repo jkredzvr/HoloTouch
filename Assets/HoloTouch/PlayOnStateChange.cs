270e8a8108dec98c2c045ee0ed33d1ed155f5386
jchin <justinchin.bcchs@gmail.com>
2018-04-09 14:00:05 -0600

    working test demo with 4 qr codes

---
+++ Assets/HoloTouch/PlayOnStateChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnStateChange : MonoBehaviour {
+
+    public AudioClip appearClip;
+    public AudioClip disappearClip;
+    private AudioSource source;
+
+	// Use this for initialization
+	void Awake () {
+        source = GetComponent<AudioSource>();
+        source.enabled = false;
+	}
+
+    public void Initialize(){
+        source.enabled = true;
+    }
+    
+    public void PlayOnAppear() {
+        source.clip = appearClip;
+        source.Play();
+    }
+
+    public void PlayOnDisappear() {
+        source.clip = disappearClip;
+        source.Play();
+    }
+}
---
+++ Assets/HoloTouch/TargetTrackingManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetTrackingManager : MonoBehaviour {
+    public List<TrackableMarker> trackableMarkerList;
+
+    public void AddTracker(TrackableMarker trackable) {
+        trackableMarkerList.Add(trackable);
+    }
+
+    public bool isTracked(TrackableMarker updatedTrackable) {
+        foreach (TrackableMarker trackable in trackableMarkerList) {
+            if(updatedTrackable != trackable) {
+                if ((trackable.GetTrackableState() == TrackableBehaviour.Status.DETECTED ||
+                trackable.GetTrackableState() == TrackableBehaviour.Status.TRACKED)) {
+                    return true;
+                }
+            } 
+        }
+        return false;
+    }
+
+    private TrackableMarker GetTrackedMarker() {
+        foreach (TrackableMarker trackable in trackableMarkerList) {  
+            if ((trackable.GetTrackableState() == TrackableBehaviour.Status.DETECTED ||
+            trackable.GetTrackableState() == TrackableBehaviour.Status.TRACKED)) {
+                return trackable;
+            }    
+        }
+        return null;
+    }
+
+
+    public void UpdateTrackState(TrackableMarker trackable, bool isTracked){
+        if (!isTracked) {
+            trackable.ShowRenderGameObject(false);
+            
+            //Show another Tracked Marker
+            TrackableMarker anotherTrackedMarker = GetTrackedMarker();
+            if (anotherTrackedMarker != null) {
+                anotherTrackedMarker.ShowRenderGameObject(true);
+            }
+        } else {
+            bool anyTrackedMarker = this.isTracked(trackable);
+            if (!anyTrackedMarker) {
+                Debug.Log("rendering graph");
+                trackable.ShowRenderGameObject(true);
+            }
+        }
+    }
+}
